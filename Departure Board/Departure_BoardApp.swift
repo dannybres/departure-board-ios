f7298b06d56b25a87d7cf5e0d062c9d8106dac3e
Daniel Breslan <daniel@breslan.co.uk>
2026-02-12 08:58:13 +0000

    Initial Commit

---
+++ Departure Board/Departure_BoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Departure_BoardApp.swift
+//  Departure Board
+//
+//  Created by Daniel Breslan on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Departure_BoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
